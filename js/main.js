eb76da4aa33f962a9ed0a700d6e1aec873765893
Mauricio de Britto <plurnstyle@gmail.com>
2023-02-14 14:06:07 -0300

    Revert "main branch for future branchs"
    This reverts commit 6eb8e51f345fa33daa8153add5c71a788484d99f.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.cadastrar-tarefa').on('click', function(){
+        const nameTarefa = $('#nome-Tarefa').val()
+        const li = document.createElement('li')
+        li.innerHTML = `${nameTarefa}`
+        $('ul').append(li)
+        $('#nome-Tarefa').val('')
+        $(li).click(function(){
+            $(li).toggleClass('done')
+        })
+    })
+    
+    
+})
